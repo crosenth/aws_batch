1abb1e42f8ed85b3d16a6f7dd2dc74f42f38ac4d
Chris Rosenthal <crosenth@uw.edu>
2019-11-06 16:35:30 -0800

    enable -V

--- aws_batch.py
+++ aws_batch.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
-#
-# Copyright 2013-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You may not
-# use this file except in compliance with the
-# License. A copy of the License is located at
-#
-# http://aws.amazon.com/apache2.0/
-#
-# or in the "LICENSE.txt" file accompanying this file. This file is distributed
-# on an "AS IS" BASIS, WITHOUT WARRANTIES
-# OR CONDITIONS OF ANY KIND, express or implied. See the License for the
-# specific language governing permissions and
-# limitations under the License.
-#
-# Submits an image classification training job to an AWS Batch job queue, and
-# tails the CloudWatch log output.
-#
+'''
+Given an AWS registered job-definition and optional command with inputs and
+outputs, this script moves data to and from a local filesystem into an AWS S3
+Bucket and Batch container for processing. All data written to the AWS Batch
+logging system is monitored and displayed to the user.
+
+Requirements:
+
+1. An AWS S3 Bucket
+2. awscli must be available in the container for moving data to and from
+the AWS S3 bucket
+
+Inpired from https://github.com/awslabs/aws-batch-helpers/
+blob/master/gpu-example/submit-job.py
+'''
 import argparse
 import boto3
 import datetime
 import logging
 import os
+import pkg_resources
 import subprocess
 import sys
 import time
@@ -82,11 +80,11 @@ container_parser.add_argument(
     help='container folder to execute command')
 logging_parser = parser.add_argument_group(
     title='logging and version options')
-# logging_parser.add_argument(
-#     '-V', '--version',
-#     action='version',
-#     version=pkg_resources.get_distribution('classifier').version,
-#     help='Print the version number and exit')
+logging_parser.add_argument(
+    '-V', '--version',
+    action='version',
+    version=pkg_resources.get_distribution('aws_batch').version,
+    help='Print the version number and exit')
 logging_parser.add_argument(
     '-l', '--log',
     metavar='',
